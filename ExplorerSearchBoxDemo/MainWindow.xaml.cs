1f5763aa085e81c52f7b38b87bf04edbe3a42360
RapeCat <rapecat@protonmail.com>
2019-08-08 11:40:58 +0800

    Bindable hint text support.

--- ExplorerSearchBoxDemo/MainWindow.xaml.cs
+++ ExplorerSearchBoxDemo/MainWindow.xaml.cs
@@ -2,17 +2,17 @@
 using System.Windows.Input;
 
 namespace ExplorerSearchBoxDemo {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
+
     public partial class MainWindow : Window {
 
+        // Not necessarily a view model.
         private readonly MeowModel model;
 
         // Field for click-outsides handling.
         private readonly MouseButtonEventHandler windowWideMouseButtonEventHandler;
 
         static MainWindow() {
+            // Bind for Ctrl + F => Activate search.
             CommandManager.RegisterClassCommandBinding(
                 typeof(MainWindow),
                 new CommandBinding(
@@ -28,23 +28,34 @@ namespace ExplorerSearchBoxDemo {
         }
 
         public MainWindow() {
+
             InitializeComponent();
+
             model = new MeowModel();
             DataContext = model;
+
+            // Click events in the window will be previewed by
+            // function OnWindowWideMouseEvent (defined later)
+            // when the handler is on. Now it's off.
             windowWideMouseButtonEventHandler = new MouseButtonEventHandler(OnWindowWideMouseEvent);
+
             SearchBox.DefaultFocusedElement = ListBox;
         }
 
+        // This would only be on whenever search box is focused.
         private void OnWindowWideMouseEvent(object sender, MouseButtonEventArgs e) {
+            // By clicking outsides the search box deactivate the search box.
             if (!SearchBox.IsMouseOver) SearchBox.DeactivateSearch();
         }
 
+        // The two come in pair.
         private void SearchBox_SearchTextFocused(object sender, System.EventArgs e)
             => AddHandler(Window.PreviewMouseDownEvent, windowWideMouseButtonEventHandler);
 
         private void SearchBox_SearchTextUnfocused(object sender, System.EventArgs e)
             => RemoveHandler(Window.PreviewMouseDownEvent, windowWideMouseButtonEventHandler);
 
+        // Update filter text of the naive model.
         private void SearchBox_SearchRequested(object sender, string e) => model.FilterText = e;
     }
 }
